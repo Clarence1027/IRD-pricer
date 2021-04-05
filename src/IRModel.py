c84f7775479f59983a38d921d63b9c7d764b13e0
Clarence1027 <56453292+Clarence1027@users.noreply.github.com>
2021-04-05 16:49:39 -0400

    Pricer

--- src/IRModel.py
+++ src/IRModel.py
@@ -1,10 +1,11 @@
 from . import Curves
 import pandas as pd
+import numpy as np
 
 
 class BondOptionPricer:
 
-    def __init__(self,data_, model_='V', intp_='CS', opt_=[0.567, 6, 8], step_=1 / 12):
+    def __init__(self, data_, model_='V', intp_='CS', opt_=[0.567, 6, 8], step_=1 / 12):
         '''
         paras:
                 @opt_: option data, opt[0]: strike; opt[1]: opt maturity; opt[2]: bond maturity
@@ -24,6 +25,7 @@ class BondOptionPricer:
         tenor = [Curves.TsyYldCurve.getMaturityInYears(
             x) for x in CMT.columns[1:]]
         fail_dates = []
+
         def spot_cooking_from_CMT(x):
             par_curve = Curves.TsyYldCurve(tenor, x[1:])
             try:
@@ -47,10 +49,10 @@ class BondOptionPricer:
         # interpolate spot curve with new tenor
         tenor = [
             i * self.step for i in range(int(np.ceil(self.opt[2] / self.step)))]
-        f = lambda x: Curves.SpotRateCurve(spot.columns,x)(tenor)
-        df = self.spot.apply(f,axis=1,result_type='expand')
+        f = lambda x: Curves.SpotRateCurve(spot.columns, x)(tenor)
+        df = self.spot.apply(f, axis=1, result_type='expand')
         df.columns = tenor
-        return pd.DataFrame(zip(df.columns,np.log(df[-window_len:]/df[-window_len:].shift(1)).std(axis=0)),columns=['tenor','vol'])
+        return pd.DataFrame(zip(df.columns, np.log(df[-window_len:] / df[-window_len:].shift(1)).std(axis=0)), columns=['tenor', 'vol'])
 
     def calibrateIRModel(self):
         # create, calibrate and save IR model
@@ -73,16 +75,21 @@ class Vasicek(IRModel):
         super().__init__()
 
     def __call__(self):
+
         pass
 
     def calibrate(self, init_arg):
         # for vasicek, 3 parameters should be determined
         #
+        B = (1 - np.exp(-a * tao)) / a
+        A = np.exp((B - T + tao) * (a**2 * b - sig**2 / 2) /
+                   a**2 - sig**2 * B**2 / a / 4)
+
         pass
 
     @classmethod
     def analytic(cls):
-    	pass
+        pass
 
 
 if __name__ == '__main__':
