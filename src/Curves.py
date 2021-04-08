66d84dfae7f5ce9dd979f7e6d0536205959abfe3
Clarence1027 <56453292+Clarence1027@users.noreply.github.com>
2021-04-08 00:56:56 -0400

    update

--- src/Curves.py
+++ src/Curves.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
 import re
-from scipy.optimize import newton, brentq
+from scipy.optimize import newton, brentq, minimize
 
 
 class Curve:
@@ -57,8 +57,8 @@ class SpotRateCurve (Curve):
         super().__init__(x, y)
 
     def __call__(self, date, method='linear'):
-        f = interp1d(self.x, self.y, kind=method, fill_value='extrapolate')
-        return f(date)
+        # f = interp1d(self.x, self.y, kind=method, fill_value='extrapolate')
+        return interpolation(self.x, self.y, method, date)
 
     def generateForwardCurve(self):
         forwardrate = [self.y[0]]
@@ -121,14 +121,85 @@ class TsyYldCurve (Curve):
     def getCurveDate(cls):
         return cls.curveDate
 
+
+def interpolation(tenor, y, method, tenor_new):  # method can be either 'Cubic' or 'NS'
+    if method == 'Cubic':
+
+        # determine interval length
+        n = int(np.floor(len(tenor) / 3))
+
+        def Cubic(x, t):
+            return x[0] + x[1] * t + x[2] * t**2 + x[3] * t**3
+
+        def fun(x):
+            tmp1 = np.array([Cubic(x[:4], tenor[i]) - y[i] for i in range(n)])
+            tmp2 = np.array([Cubic(x[4:8], tenor[i]) - y[i]
+                             for i in range(n, 2 * n)])
+            tmp3 = np.array([Cubic(x[8:], tenor[i]) - y[i]
+                             for i in range(2 * n, len(tenor))])
+
+            return (tmp1**2).sum() + (tmp2**2).sum() + (tmp3**2).sum()
+
+        def F1(x, t):  # first derivative
+            return x[1] + 2 * x[2] * t + 3 * x[3] * t**2
+
+        def F2(x, t):  # second derivative
+            return 2 * x[2] + 6 * x[3] * t
+
+        cons = ({'type': 'eq', 'fun': lambda x: Cubic(x[:4], tenor[n - 1]) - Cubic(x[4:8], tenor[n - 1])},
+                {'type': 'eq', 'fun': lambda x: Cubic(
+                    x[4:8], tenor[2 * n - 1]) - Cubic(x[8:], tenor[2 * n - 1])},
+                {'type': 'eq', 'fun': lambda x: F1(
+                    x[:4], tenor[n - 1]) - F1(x[4:8], tenor[n - 1])},
+                {'type': 'eq', 'fun': lambda x: F1(
+                    x[4:8], tenor[2 * n - 1]) - F1(x[8:], tenor[2 * n - 1])},
+                {'type': 'eq', 'fun': lambda x: F2(
+                    x[:4], tenor[n - 1]) - F2(x[4:8], tenor[n - 1])},
+                {'type': 'eq', 'fun': lambda x: F2(x[4:8], tenor[2 * n - 1]) - F2(x[8:], tenor[2 * n - 1])})
+
+        x0 = [0.5 for i in range(12)]
+        res = minimize(fun, x0, constraints=cons)
+        n_ = int(np.floor(len(tenor_new) / 3))
+        model = [Cubic(res.x[:4], tenor_new[i]) for i in range(n_)]
+        model += [Cubic(res.x[4:8], tenor_new[i]) for i in range(n_, 2 * n_)]
+        model += [Cubic(res.x[8:], tenor_new[i])
+                  for i in range(2 * n_, len(tenor_new))]
+        if not res.success:
+            return None
+        return np.array(model)
+
+    elif method == 'NS':
+        n = len(tenor)
+
+        def NS(x, t):
+            exp = np.exp(-t / x[3])
+            return x[0] + x[1] * ((1 - exp) / (t / x[3])) + x[2] * (((1 - exp) / (t / x[3])) - exp)
+
+        def fun(x):
+            tmp = np.array([NS(x, tenor[i]) - y[i] for i in range(n)])
+            return (tmp**2).sum()
+
+        x0 = [0.1 for i in range(4)]
+        res = minimize(fun, x0)
+        # print(res)
+        if not res.success:
+            f = interp1d(tenor, y, kind='cubic', fill_value='extrapolate')
+            return f(tenor_new)
+        return np.array([NS(res.x, tenor_new[i]) for i in range(len(tenor_new))])
+
 if __name__ == '__main__':
     # print("The bootstrapped forward rate curve is\n {}".format(np.array((TsyYldCurve().getForwardCurve().x,TsyYldCurve().getForwardCurve().y))))
     # print("The spot rate curve is\n {}".format(np.array((TsyYldCurve().getForwardCurve().generateSpotCurve().x,TsyYldCurve().getForwardCurve().generateSpotCurve().y))))
-    CMT_df = pd.read_csv('../data/CMT/CMT/CMT.csv')
-    par = CMT_df.values[0, 1:]
-    tenor = [TsyYldCurve.getMaturityInYears(x) for x in CMT_df.columns[1:]]
-    par_curve = TsyYldCurve(tenor, par)
-    spot_curve = par_curve.getForwardCurve().generateSpotCurve()
-    print(par)
-    print(tenor)
-    print(spot_curve.y)
+    # CMT_df = pd.read_csv('../data/CMT/CMT/CMT.csv')
+    # par = CMT_df.values[0, 1:]
+    # tenor = [TsyYldCurve.getMaturityInYears(x) for x in CMT_df.columns[1:]]
+    # par_curve = TsyYldCurve(tenor, par)
+    # spot_curve = par_curve.getForwardCurve().generateSpotCurve()
+    # print(par)
+    # print(tenor)
+    # print(spot_curve.y)
+
+    tenor = np.array([1 / 12, 0.25, 0.5, 1, 2, 3, 5, 7, 10, 20, 30])
+    yields = np.array([0.04, 0.0600005, 0.110007, 0.240086, 0.691685,
+                       1.15725, 2.16042, 2.86866,  3.57557,  4.57708,  4.8913])
+    print(interpolation(tenor, yields, 'NS', tenor))
